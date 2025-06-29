d0ce0ce3640233d1584181d545aa4caae13d9796
Michael Lieberman <michaellieberman44@gmail.com>
2025-06-28 23:00:36 -0400

    Get positon and last name routes

--- backend/src/routes/players.ts
+++ backend/src/routes/players.ts
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/', async (req: Request, res: Response) => {
   try {
     const db = await getDbConnection();
-    const teams = await db.all('SELECT * FROM players');
-    res.json(teams);
+    const players = await db.all('SELECT * FROM players');
+    res.json(players);
   } catch (error) {
     console.error('Error fetching teams:', error);
     res.status(500).json({ error: 'Failed to fetch teams' });
@@ -21,11 +21,39 @@ router.get('/nfl/:nfl_team', async (req: Request, res: Response) => {
 
   try {
     const db = await getDbConnection();
-    const players = await db.all('SELECT * FROM players WHERE nfl_team = ?', [nfl_team]);
+    const players = await db.all('SELECT * FROM players WHERE LOWER(nfl_team) = LOWER(?)', [nfl_team]);
     res.json(players);
   } catch (error) {
     console.error('Error fetching players by team:', error);
     res.status(500).json({ error: 'Failed to fetch players by team' });
+  }
+});
+
+// Get players by position
+router.get('/position/:position', async (req: Request, res: Response) => {
+  const { position } = req.params;
+
+  try {
+    const db = await getDbConnection();
+    const players = await db.all('SELECT * FROM players WHERE LOWER(position) = LOWER(?)', [position]);
+    res.json(players);
+  } catch (error) {
+    console.error('Error fetching players by position:', error);
+    res.status(500).json({ error: 'Failed to fetch players by position' });
+  }
+});
+
+// should probably search for first or last name or both
+// Get players by last name 
+router.get('/name/:last_name', async (req: Request, res: Response) => {
+  const { last_name } = req.params;
+  try {
+    const db = await getDbConnection();
+    const players = await db.all('SELECT * FROM players WHERE LOWER(last_name) = LOWER(?)', [last_name]);
+    res.json(players);
+  } catch (error) {
+    console.error('Error fetching players by last name:', error);
+    res.status(500).json({ error: 'Failed to fetch players by last name' });
   }
 });
 
